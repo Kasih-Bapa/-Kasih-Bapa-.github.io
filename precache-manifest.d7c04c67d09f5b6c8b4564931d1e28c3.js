be1b35c7a62f50ad568f007d73d0d407bd75b4d6
Kasih-Bapa <nicolas.ivan896@gmail.com>
2019-11-21 09:10:14 +0700

    Babi

---
+++ precache-manifest.d7c04c67d09f5b6c8b4564931d1e28c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "414dcd98cbf0dae3d0fb",
+    "url": "/css/about.3b3c9ddb.css"
+  },
+  {
+    "revision": "ab613b4d4a2b77f94134",
+    "url": "/css/chunk-vendors.ec4de8ca.css"
+  },
+  {
+    "revision": "849e73227217cc392d4f6fe357b334d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "414dcd98cbf0dae3d0fb",
+    "url": "/js/about.a2bc3913.js"
+  },
+  {
+    "revision": "5a7b5536dd98c0c77086",
+    "url": "/js/app.c9f16600.js"
+  },
+  {
+    "revision": "ab613b4d4a2b77f94134",
+    "url": "/js/chunk-vendors.3e161523.js"
+  },
+  {
+    "revision": "555ec9aa3d17479e08f99f3b8bebdfe0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
